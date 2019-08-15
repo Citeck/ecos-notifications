82d80d6ea2c78ed1a885a1dfdf6994d37ed58d62
Roman Makarskiy <Romanchabest55@gmail.com>
2019-08-15 11:47:04 +0300

    Get credentials from classpath or absolute path

--- src/main/java/ru/citeck/ecos/notifications/config/FirebaseConfig.java
+++ src/main/java/ru/citeck/ecos/notifications/config/FirebaseConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 
 import javax.annotation.PostConstruct;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -27,22 +28,35 @@ public class FirebaseConfig {
     @PostConstruct
     public void initFirebaseApp() {
         FirebaseOptions options = null;
+
+        log.info("Init firebase app from credentials: " + firebaseProps.getCredentialClassPath());
+
         try (InputStream credentials = new ClassPathResource(
             firebaseProps.getCredentialClassPath()).getInputStream()) {
-
-            options = new FirebaseOptions.Builder()
-                .setCredentials(GoogleCredentials.fromStream(credentials))
-                .setDatabaseUrl(firebaseProps.getDataBaseUrl())
-                .build();
-
+            options = getOptionsFromInputStream(credentials);
         } catch (IOException e) {
-            log.error("Failed to get credentials for firebase", e);
+            log.info("Credentials from class path not found, trying to get from absolute path...");
+
+            try (InputStream credentials = new FileInputStream(firebaseProps.getCredentialClassPath())) {
+                options = getOptionsFromInputStream(credentials);
+            } catch (IOException ex) {
+                log.error("Failed to get credentials for firebase", e);
+            }
         }
 
         if (options != null) {
+            log.info("Firebase App successfully initialize.");
             FirebaseApp.initializeApp(options);
+        } else {
+            log.warn("Firebase App not initialized.");
         }
+    }
 
+    private FirebaseOptions getOptionsFromInputStream(InputStream credentials) throws IOException {
+        return new FirebaseOptions.Builder()
+            .setCredentials(GoogleCredentials.fromStream(credentials))
+            .setDatabaseUrl(firebaseProps.getDataBaseUrl())
+            .build();
     }
 
 }
