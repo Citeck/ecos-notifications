535ec99736a66def940a0360e57d370c92e1042f
Roman Makarskiy <Romanchabest55@gmail.com>
2020-08-06 12:44:01 +0300

    Fix dto type

--- src/main/java/ru/citeck/ecos/notifications/domain/template/records/NotificationTemplateRecords.kt
+++ src/main/java/ru/citeck/ecos/notifications/domain/template/records/NotificationTemplateRecords.kt
@@ -9,6 +9,7 @@ import ru.citeck.ecos.commons.io.file.EcosFile
 import ru.citeck.ecos.commons.io.file.mem.EcosMemDir
 import ru.citeck.ecos.commons.json.Json.mapper
 import ru.citeck.ecos.commons.utils.ZipUtils
+import ru.citeck.ecos.notifications.domain.template.dto.NotificationTemplateDto
 import ru.citeck.ecos.notifications.domain.template.dto.NotificationTemplateWithMeta
 import ru.citeck.ecos.notifications.domain.template.dto.TemplateDataDto
 import ru.citeck.ecos.notifications.domain.template.getContentBytesFromBase64ObjectData
@@ -185,7 +186,7 @@ class NotificationTemplateRecords(val templateService: NotificationTemplateServi
             get() = let {
                 val memDir = EcosMemDir()
 
-                val metaDto = NotificationTemplateWithMeta(this)
+                val metaDto = NotificationTemplateDto(this)
                 val prettyString = mapper.toPrettyString(metaDto)
 
                 mapper.toBytes(prettyString)?.let {
