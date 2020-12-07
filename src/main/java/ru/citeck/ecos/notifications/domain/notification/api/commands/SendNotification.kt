91b43303292a6bb61ed57b9d9982c8a1397c5a27
Zaytsev Aleksey <aleksey.zaytsev@citeck.ru>
2020-12-07 15:30:16 +0700

    ECOSCOM-4048 - Ecos notifications. Add the ability to check a predicate in a multi templates Return changes from dev

--- src/main/java/ru/citeck/ecos/notifications/domain/notification/api/commands/SendNotification.kt
+++ src/main/java/ru/citeck/ecos/notifications/domain/notification/api/commands/SendNotification.kt
@@ -1,129 +1,30 @@
 package ru.citeck.ecos.notifications.domain.notification.api.commands
 
 import mu.KotlinLogging
-import org.apache.commons.lang3.LocaleUtils
-import org.apache.commons.lang3.StringUtils
 import org.springframework.stereotype.Service
 import ru.citeck.ecos.commands.CommandExecutor
- import ru.citeck.ecos.notifications.domain.notification.DEFAULT_LOCALE
-import ru.citeck.ecos.notifications.domain.notification.RawNotification
-import ru.citeck.ecos.notifications.domain.notification.predicate.MapElement
-import ru.citeck.ecos.notifications.domain.notification.service.NotificationException
-import ru.citeck.ecos.notifications.domain.notification.service.NotificationService
-import ru.citeck.ecos.notifications.domain.template.dto.NotificationTemplateWithMeta
-import ru.citeck.ecos.notifications.domain.template.service.NotificationTemplateService
+import ru.citeck.ecos.notifications.domain.notification.service.FailureNotificationService
 import ru.citeck.ecos.notifications.lib.command.SendNotificationCommand
 import ru.citeck.ecos.notifications.lib.command.SendNotificationResult
-import ru.citeck.ecos.records2.RecordRef
-import ru.citeck.ecos.records2.predicate.PredicateService
-import ru.citeck.ecos.records2.predicate.model.Predicate
-import java.util.*
-
-private const val ECOS_TYPE_ID_KEY = "_etype?id"
 
 @Service
 class SendNotificationCommandExecutor(
-    val notificationService: NotificationService,
-    val notificationTemplateService: NotificationTemplateService,
-    var predicateService: PredicateService
+    val unsafeSendNotificationCommandExecutor: UnsafeSendNotificationCommandExecutor,
+    val failureNotificationService: FailureNotificationService
 ) : CommandExecutor<SendNotificationCommand> {
 
+    companion object {
     private val log = KotlinLogging.logger {}
+    }
 
     override fun execute(command: SendNotificationCommand): Any? {
-        val baseTemplate = getTemplateMetaById(command.templateRef.id)
+        return try {
+            unsafeSendNotificationCommandExecutor.execute(command)
+        } catch (e: Exception) {
+            log.error("Failed execute notification command", e)
 
-        val template = resolveMultiTemplate(
-            baseTemplate = baseTemplate,
-            recordEcosTypeId = getRecordEcosTypeByIncomeModel(command.model),
-            attributes = MapElement(command.model)
-        )
-
-        val requiredModel = mutableMapOf<String, String>()
-        baseTemplate.model?.let { requiredModel.putAll(it) }
-        if (!Objects.equals(baseTemplate.id, template.id)) {
-            template.model?.let { requiredModel.putAll(it) }
-        }
-
-        val filledModel = resolveCompletedModel(
-            requiredModel = requiredModel,
-            incomeFilledModel = command.model
-        )
-
-        val locale = if (command.lang.isEmpty()) DEFAULT_LOCALE else LocaleUtils
-            .toLocale(command.lang)
-
-        val notification = RawNotification(
-            template = template,
-            type = command.type,
-            locale = locale,
-            recipients = command.recipients,
-            model = filledModel,
-            from = command.from,
-            cc = command.cc,
-            bcc = command.bcc
-        )
-
-        notificationService.send(notification)
-
-        return SendNotificationResult("ok", "")
-    }
-
-    private fun resolveMultiTemplate(
-        baseTemplate: NotificationTemplateWithMeta,
-        recordEcosTypeId: String,
-        attributes: MapElement
-    ): NotificationTemplateWithMeta {
-        if (StringUtils.isBlank(recordEcosTypeId)) {
-            return baseTemplate
-        }
-
-        baseTemplate.multiTemplateConfig?.forEach { it ->
-            it.type?.let { typeRef ->
-                val checkType = RecordRef.valueOf(recordEcosTypeId) == typeRef
-                if (checkType && checkPredicate(it.condition, attributes)) {
-                    val template = it.template ?: throw NotificationException(
-                        "Multi template ref is null. Base template ref: $baseTemplate"
-                    )
-                    return resolveMultiTemplate(getTemplateMetaById(template.id), recordEcosTypeId, attributes)
-                }
-            }
-        }
-
-        return baseTemplate
-    }
-
-    private fun checkPredicate(condition: Predicate?, attributes: MapElement): Boolean {
-        condition?.let {
-            return predicateService.isMatch(attributes, it)
-        }
-        return true
-    }
-
-    private fun getRecordEcosTypeByIncomeModel(incomeFilledModel: Map<String, Any>): String {
-        val value = incomeFilledModel[ECOS_TYPE_ID_KEY] ?: return ""
-        return value.toString()
-    }
-
-    private fun resolveCompletedModel(requiredModel: Map<String, String>, incomeFilledModel: Map<String, Any>): Map<String, Any> {
-        val filledModel = mutableMapOf<String, Any>()
-        val prefilledModel = incomeFilledModel.toMutableMap()
-
-        requiredModel.forEach { (attrKey, attrValue) ->
-            incomeFilledModel[attrValue]?.let {
-                filledModel[attrKey] = it
-                prefilledModel.remove(attrValue)
-            }
-        }
-
-        filledModel.putAll(prefilledModel)
-
-        return filledModel
-    }
-
-    private fun getTemplateMetaById(id: String): NotificationTemplateWithMeta {
-        return notificationTemplateService.findById(id).orElseThrow {
-            NotificationException("Template with id: <$id> not found}")
+            failureNotificationService.holdFailure(command, e)
+            SendNotificationResult("error", e.message ?: "")
         }
     }
 
