8c8b540f134f8130c4002a28f0ab4a295c5641c1
Roman.Makarskiy <roman.makarskiy@citeck.ru>
2021-09-21 13:58:41 +0300

    migrate to new events version

--- src/test/java/ru/citeck/ecos/notifications/config/EventServiceDummyConfig.kt
+++ src/test/java/ru/citeck/ecos/notifications/config/EventServiceDummyConfig.kt
@@ -2,17 +2,14 @@ package ru.citeck.ecos.notifications.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import ru.citeck.ecos.events2.EventService
-import ru.citeck.ecos.events2.EventServiceFactory
-import ru.citeck.ecos.records3.RecordsServiceFactory
+import ru.citeck.ecos.events2.EventsService
+import ru.citeck.ecos.events2.EventsServiceFactory
 
 @Configuration
-class EventServiceDummyConfig(
-    recordsServiceFactory: RecordsServiceFactory
-) : EventServiceFactory(recordsServiceFactory) {
+class EventServiceDummyConfig : EventsServiceFactory() {
 
     @Bean
-    override fun createEventService(): EventService {
-        return super.createEventService()
+    override fun createEventsService(): EventsService {
+        return super.createEventsService()
     }
 }
