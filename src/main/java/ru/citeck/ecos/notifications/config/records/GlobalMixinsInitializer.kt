ef1996992c16e2612164791c6d85da869ae854ef
Pavel Simonov <pavel.simonov@citeck.ru>
2021-09-28 09:36:53 +0300

    ECOSCOM-4404 - ecos events
    add type mixin

---
+++ src/main/java/ru/citeck/ecos/notifications/config/records/GlobalMixinsInitializer.kt
@@ -0,0 +1,21 @@
+package ru.citeck.ecos.notifications.config.records
+
+import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.stereotype.Component
+import ru.citeck.ecos.model.lib.ModelServiceFactory
+import ru.citeck.ecos.model.lib.type.api.records.TypesMixin
+import ru.citeck.ecos.records3.record.dao.AbstractRecordsDao
+import javax.annotation.PostConstruct
+
+@Component
+class GlobalMixinsInitializer(
+    @Qualifier("remoteTypesSyncRecordsDao")
+    val typesRecordsDao: AbstractRecordsDao,
+    val modelServices: ModelServiceFactory
+) {
+
+    @PostConstruct
+    fun init() {
+        typesRecordsDao.addAttributesMixin(TypesMixin(modelServices))
+    }
+}
--- src/main/java/ru/citeck/ecos/notifications/config/records/RecordsConfig.java
+++ src/main/java/ru/citeck/ecos/notifications/config/records/RecordsConfig.java
@@ -1,8 +1,10 @@
-package ru.citeck.ecos.notifications.config;
+package ru.citeck.ecos.notifications.config.records;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import ru.citeck.ecos.notifications.config.ApplicationProperties;
+import ru.citeck.ecos.records2.source.dao.local.RemoteSyncRecordsDao;
 import ru.citeck.ecos.records3.RecordsProperties;
 
 import java.util.Collections;
@@ -35,6 +37,11 @@ public class RecordsConfig {
         props.setApps(Collections.singletonMap(ALFRESCO_SOURCE_ID, alfrescoApp));
 
         return props;
+    }
+
+    @Bean(name = "remoteTypesSyncRecordsDao")
+    public RemoteSyncRecordsDao<EcosTypeInfo> createRemoteTypesSyncRecordsDao() {
+        return new RemoteSyncRecordsDao<>("emodel/type", "emodel/rtype", EcosTypeInfo.class);
     }
 
 }
