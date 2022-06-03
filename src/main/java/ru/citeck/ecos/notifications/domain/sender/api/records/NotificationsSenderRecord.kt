4bebf9d300175595fada2966267881e4d492b752
Elena Shushunova <elena.shushunova@citeck.ru>
2022-06-03 17:54:24 +0500

    ECOSCOM-4679 - Add notification senders support in notifications app Display name fix

--- src/main/java/ru/citeck/ecos/notifications/domain/sender/api/records/NotificationsSenderRecord.kt
+++ src/main/java/ru/citeck/ecos/notifications/domain/sender/api/records/NotificationsSenderRecord.kt
@@ -2,6 +2,8 @@ package ru.citeck.ecos.notifications.domain.sender.api.records
 
 import ecos.com.fasterxml.jackson210.annotation.JsonValue
 import lombok.Data
+import org.apache.commons.lang.LocaleUtils
+import ru.citeck.ecos.commons.data.MLText
 import ru.citeck.ecos.commons.data.ObjectData
 import ru.citeck.ecos.commons.json.Json.mapper
 import ru.citeck.ecos.commons.json.YamlUtils.toNonDefaultString
@@ -12,8 +14,10 @@ import ru.citeck.ecos.records2.RecordConstants
 import ru.citeck.ecos.records2.RecordRef
 import ru.citeck.ecos.records2.predicate.model.Predicate
 import ru.citeck.ecos.records3.record.atts.schema.annotation.AttName
+import ru.citeck.ecos.records3.record.request.RequestContext
 import java.nio.charset.StandardCharsets
 import java.time.Instant
+import java.util.*
 
 /**
  * Class supports uploading board from yml-file,
@@ -97,4 +101,13 @@ class NotificationsSenderRecord (
     @get:AttName(RecordConstants.ATT_CREATOR)
     val recordCreator: String?
         get() = creator
+
+    @get:AttName(RecordConstants.ATT_DISP)
+    val disp: String?
+        get() = let {
+            val dispName = MLText(Pair(Locale.ENGLISH, "Sender $id $senderType"),
+                Pair(Locale("ru"),
+                    "\u041e\u0442\u043f\u0440\u0430\u0432\u0438\u0442\u0435\u043b\u044c $id $senderType"))
+            return dispName.get(RequestContext.getLocale())
+        }
 }
