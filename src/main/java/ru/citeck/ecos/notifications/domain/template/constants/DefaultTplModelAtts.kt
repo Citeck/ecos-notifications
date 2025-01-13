3bdcc7a2394bb650cbab0e6fc88c2d96e02427d5
Pavel Simonov <pavel.simonov@citeck.ru>
2025-01-13 14:24:07 +0700

    ECOSCOM-5572 - Add workspace support for notification templates. Release 2.21.1

---
+++ src/main/java/ru/citeck/ecos/notifications/domain/template/constants/DefaultTplModelAtts.kt
@@ -0,0 +1,6 @@
+package ru.citeck.ecos.notifications.domain.template.constants
+
+object DefaultTplModelAtts {
+    const val ATT_WORKSPACE = "_workspace"
+    const val ATT_TO_LOAD_WORKSPACE = "_workspace?localId!_type.defaultWorkspace?str"
+}
--- src/main/java/ru/citeck/ecos/notifications/freemarker/beans/MetaAccessor.kt
+++ src/main/java/ru/citeck/ecos/notifications/freemarker/beans/MetaAccessor.kt
@@ -1,34 +1,27 @@
 package ru.citeck.ecos.notifications.freemarker.beans
 
 import org.springframework.stereotype.Component
+import ru.citeck.ecos.context.lib.ctx.EcosContext
 import ru.citeck.ecos.notifications.freemarker.InjectedFreemarkerBean
+import ru.citeck.ecos.notifications.freemarker.TemplateProcCtxKey
 import ru.citeck.ecos.webapp.api.properties.EcosWebAppProps
 
 private const val ID = "meta"
 
 @Component
-class MetaAccessor(private val ecosWebAppProps: EcosWebAppProps) : InjectedFreemarkerBean {
-
-    companion object {
-        private val customWebUrl = ThreadLocal.withInitial { "" }
-
-        fun <T> doWithCustomWebUrl(url: String, action: () -> T): T {
-            val prev = customWebUrl.get()
-            customWebUrl.set(url)
-            try {
-                return action.invoke()
-            } finally {
-                customWebUrl.set(prev)
-            }
-        }
-    }
+class MetaAccessor(
+    private val ecosWebAppProps: EcosWebAppProps,
+    private val ecosContext: EcosContext
+) : InjectedFreemarkerBean {
 
     override fun getId(): String {
         return ID
     }
 
     fun getWebUrl(): String {
-        var url = customWebUrl.get().ifBlank { ecosWebAppProps.webUrl }
+        val customWebUrl = ecosContext.get(TemplateProcCtxKey.CUSTOM_WEB_URL) as? String ?: ""
+
+        var url = customWebUrl.ifBlank { ecosWebAppProps.webUrl }
         if (!url.endsWith("/")) {
             url += "/"
         }
